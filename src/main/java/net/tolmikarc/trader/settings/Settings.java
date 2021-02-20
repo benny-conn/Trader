e350ef63f675762e7b458da2e548797c3f8fd52a
Tolmikarc <benny@bennyc.io>
2021-02-19 20:49:45 -0800

    Remove permission from command

--- src/main/java/net/tolmikarc/trader/settings/Settings.java
+++ src/main/java/net/tolmikarc/trader/settings/Settings.java
@@ -11,10 +11,12 @@ public class Settings extends SimpleSettings {
 
 	public static Boolean SHIFT_CLICK_TRADE;
 	public static Boolean ECONOMY_ENABLED;
+	public static Boolean LOCK_ON_CONFIRM;
 
 	private static void init() {
 		SHIFT_CLICK_TRADE = getBoolean("Shift_Click_Trade");
 		ECONOMY_ENABLED = getBoolean("Economy_Enabled");
+		LOCK_ON_CONFIRM = getBoolean("Lock_On_Confirm");
 	}
 
 
